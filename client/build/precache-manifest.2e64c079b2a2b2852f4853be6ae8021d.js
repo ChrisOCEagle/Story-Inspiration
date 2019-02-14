bb9c27f4b6e516509a9424945c8438891ce40a49
ChrisOCEagle <christopherdavyydoconnell@yahoo.com>
2019-02-14 18:41:52 -0500

    deploying

---
+++ client/build/precache-manifest.2e64c079b2a2b2852f4853be6ae8021d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3411cc56ba73f22f7c5ee89944a9cdd",
+    "url": "/static/media/story-prompt-logo.c3411cc5.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0cc941965bf010e57b50",
+    "url": "/static/js/main.0cc94196.chunk.js"
+  },
+  {
+    "revision": "6d706e4f38faee6d6fc7",
+    "url": "/static/js/1.6d706e4f.chunk.js"
+  },
+  {
+    "revision": "0cc941965bf010e57b50",
+    "url": "/static/css/main.29c15675.chunk.css"
+  },
+  {
+    "revision": "baae33480bce9fc5fe00bd1bc2837dc4",
+    "url": "/index.html"
+  }
+];
